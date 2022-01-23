8ad5e1c2758eb7b89a41c4b0a54c66967326ef37
chickensalt <nsdj.sharma@gmail.com>
2022-01-23 17:23:40 +1100

    test 2

--- lib/posts.ts
+++ lib/posts.ts
@@ -7,7 +7,6 @@ import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import path from "path";
-import { exec } from "child_process";
 
 export interface PostMeta {
   title: string;
@@ -17,14 +16,7 @@ export interface PostMeta {
 }
 
 export const getAllPosts = () => {
-  exec(
-    `tree ${path.join(__dirname, "../../../")} -I 'node_modules'`,
-    (error, stdout, stderr) => {
-      if (error) console.log(error.message);
-      if (stderr) console.log(stderr);
-      if (stdout) console.log(stdout);
-    }
-  );
+  console.log(readdirSync(path.join(__dirname, "../../../")));
   return readdirSync(path.join(__dirname, "../../../blog/")).map((file) =>
     matter(readFileSync(path.join(__dirname, `../../../blog/${file}`), "utf-8"))
   );
