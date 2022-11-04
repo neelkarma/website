e4cecb9483bb29e2a7127ae8ffa5205e763b1611
chickensalt <nsdj.sharma@gmail.com>
2022-11-04 22:49:38 +1100

    add more stuff in /uses

--- src/routes/uses/+page.svelte
+++ src/routes/uses/+page.svelte
@@ -8,6 +8,18 @@
 
 <h3><a href="/" use:bounce>back</a></h3>
 <h3 use:bounce>vscode</h3>
+<h3 use:bounce>vivaldi</h3>
+<h3 use:bounce>notion</h3>
+<h3 use:bounce>ticktick</h3>
+<h3 use:bounce>wpilib</h3>
+<h3 use:bounce>arduino</h3>
+<h3 use:bounce>raspberry pi</h3>
+<h3 use:bounce>git</h3>
+<h3 use:bounce>github</h3>
+<h3 use:bounce>gitlab</h3>
+<h3 use:bounce>powertoys</h3>
+<h3 use:bounce>python</h3>
+<h3 use:bounce>qalculate</h3>
 <h3 use:bounce>svelte</h3>
 <h3 use:bounce>sveltekit</h3>
 <h3 use:bounce>rust</h3>
