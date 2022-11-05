49a347acdddd8e042ad40abf550d6961550b4cb4
chickensalt <nsdj.sharma@gmail.com>
2022-11-05 23:58:34 +1100

    add unemployment and discord

---
+++ src/components/DiscordTag.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { bounce } from "$lib/bounce";
+
+  let content = "discord";
+
+  const handleClick = async () => {
+    await navigator.clipboard.writeText("iamkneel#2359");
+    content = "tag copied";
+    setTimeout(() => (content = "discord"), 1000);
+  };
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<h2 role="button" use:bounce on:click={handleClick}>{content}</h2>
+
+<style>
+  h2 {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { bounce } from "$lib/bounce";
   import NowPlaying from "../components/NowPlaying.svelte";
+  import DiscordTag from "../components/DiscordTag.svelte";
 </script>
 
 <head>
@@ -12,11 +13,13 @@
 <h4 use:bounce>i like listening to music</h4>
 <h4 use:bounce>honestly, i like computers in general</h4>
 <h4 use:bounce>i like playing video games</h4>
+<h3 use:bounce>currently unemployed :&lpar;</h3>
 <h2 use:bounce>have fun trying to navigate</h2>
 <h2><a href="https://www.github.com/neelkarma" use:bounce>github</a></h2>
 <h2>
   <a href="https://www.github.com/neelkarma/website" use:bounce>source</a>
 </h2>
 <h2><a href="/uses" use:bounce>uses</a></h2>
+<DiscordTag />
 <NowPlaying />
 <h1 use:bounce>iamkneel</h1>
