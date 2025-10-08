d5ab3ef805ef0452067184d8679f4da229442cf7
Neel Sharma <nsdj.sharma@gmail.com>
2025-10-08 23:58:56 +1100

    Add 404 page for cloudflare pages

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="font-header text-2xl font-bold">Error 404</h1>
+
+<p>
+  Looks like the page you were looking for doesn't exist! <a
+    href="/"
+    class="underline decoration-zinc-600 underline-offset-4 hover:decoration-zinc-200">Go home</a
+  >
+</p>
