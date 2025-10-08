96bcc7be95db10c9cc591c39f9ddd0270a2481ad
Neel Sharma <nsdj.sharma@gmail.com>
2025-10-09 00:28:14 +1100

    fix spacing and title of 404 page

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,8 +1,13 @@
-<h1 class="font-header text-2xl font-bold">Error 404</h1>
+<svelte:head>
+  <title>iamkneel | 404</title>
+</svelte:head>
 
-<p>
-  Looks like the page you were looking for doesn't exist! <a
-    href="/"
-    class="underline decoration-zinc-600 underline-offset-4 hover:decoration-zinc-200">Go home</a
-  >
-</p>
+<div class="space-y-4">
+  <h1 class="font-header text-2xl font-bold">Error 404</h1>
+  <p>
+    Looks like the page you were looking for doesn't exist! <a
+      href="/"
+      class="underline decoration-zinc-600 underline-offset-4 hover:decoration-zinc-200">Go home</a
+    >
+  </p>
+</div>
