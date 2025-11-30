5668ffa6163961f1f78286da7898bb95c334a585
Neel Sharma <nsdj.sharma@gmail.com>
2025-11-30 23:24:14 +1100

    Add more spacing between writing entries

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -18,13 +18,15 @@
   <p>This blog is also available as an <TextLink href="/writing/rss.xml">RSS Feed</TextLink></p>
 </div>
 
-{#each data.posts as { date, slug, title, description }}
-  <a href="/writing/{slug}" class="group space-y-1">
-    <h2
-      class="font-header text-xl underline decoration-zinc-600 underline-offset-4 group-hover:decoration-zinc-200"
-    >
-      {title}
-    </h2>
-    <p>{DATE_FORMAT.format(date)} - {description}</p>
-  </a>
-{/each}
+<div class="flex flex-col gap-5">
+  {#each data.posts as { date, slug, title, description }}
+    <a href="/writing/{slug}" class="group space-y-1">
+      <h2
+        class="font-header text-xl underline decoration-zinc-600 underline-offset-4 group-hover:decoration-zinc-200"
+      >
+        {title}
+      </h2>
+      <p>{DATE_FORMAT.format(date)} - {description}</p>
+    </a>
+  {/each}
+</div>
