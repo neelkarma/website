cf1dab9038dfc5372c5332185e77f318643dd53f
Neel Sharma <nsdj.sharma@gmail.com>
2026-04-24 16:03:11 +1000

    Clarify /about information

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,14 +15,12 @@
   <li>🇦🇺 Self-Taught Developer, Engineer, Teacher, and Student</li>
   <li>I like making computers do cool stuff!</li>
   <li>
-    Studying Computer Science + Electrical Engineering @ <TextLink href="https://www.unsw.edu.au"
-      >UNSW</TextLink
-    >
+    Studying Computer Science @ <TextLink href="https://www.unsw.edu.au">UNSW</TextLink>
   </li>
   <li>
-    Teaching programming and robotics @ <TextLink href="https://www.stcatherines.nsw.edu.au/"
+    Was programming and robotics @ <TextLink href="https://www.stcatherines.nsw.edu.au/"
       >St Catherine's High School</TextLink
-    > and <TextLink href="https://emu-robotics.au">EMU Robotics</TextLink>
+    > and now at <TextLink href="https://emu-robotics.au">EMU Robotics</TextLink>
   </li>
   <li>Proficient in C, Python, JavaScript, TypeScript, Go, Java, and more&trade;</li>
   <li>
